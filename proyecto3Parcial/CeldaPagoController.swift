76cccdf8b932597a6bbab2754f5d96a09a3227d6
wera <69810414+natlujan@users.noreply.github.com>
2022-11-16 12:20:07 -0700

    Create CeldaPagoController.swift

---
+++ proyecto3Parcial/CeldaPagoController.swift
@@ -0,0 +1,19 @@
+//
+//  CeldaPagoController.swift
+//  proyecto3Parcial
+//
+//  Created by Alumno ULSA on 16/11/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+import UIKit
+
+class CeldaPagoController: UITableViewCell {
+    
+    @IBOutlet weak var lblNombreSemestre: UILabel!
+    @IBOutlet weak var lblConcepto: UILabel!
+    @IBOutlet weak var lblReferencia: UILabel!
+    @IBOutlet weak var lblFecha: UILabel!
+    @IBOutlet weak var lblImportePagar: UILabel!
+    @IBOutlet weak var pagado: UIImageView!
+    
+}
