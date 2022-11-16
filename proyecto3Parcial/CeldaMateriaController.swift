f3d5de6fe32035d9b1bf71cd2ddebdae57a649f8
wera <69810414+natlujan@users.noreply.github.com>
2022-11-16 12:19:22 -0700

    Update CeldaMateriaController.swift

--- proyecto3Parcial/CeldaMateriaController.swift
+++ proyecto3Parcial/CeldaMateriaController.swift
@@ -2,15 +2,16 @@
 //  CeldaMateriaController.swift
 //  proyecto3Parcial
 //
-//  Created by Alumno ULSA on 01/11/22.
+//  Created by Alumno ULSA on 16/11/22.
 //  Copyright © 2022 Alumno. All rights reserved.
 //
-
 import UIKit
 
 class CeldaMateriaController: UITableViewCell {
     
     @IBOutlet weak var lblNombre: UILabel!
-    
     @IBOutlet weak var lblHorario: UILabel!
+    @IBOutlet weak var lblSalon: UILabel!
+    
+    @IBOutlet weak var asistencia: UIImageView!
 }
